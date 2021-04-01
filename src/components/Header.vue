1a04cf5769b0e0fe57452f449190ca9471594414
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-04-01 09:05:51 +0200

    feat: add iframe & supabase

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+
+<script setup lang="ts">
+// LOGIN & LOGOUT
+// secret id 14f2f11e64315c71f33537ecfabd65c82cb69750
+// const login = async () => {
+//   const result = await props.supabase.auth.signIn({
+//     provider: 'github',
+//   })
+//   // if (error) console.error(error)
+//   console.log(result)
+// }
+// const logout = async () => {
+//   let { error } = await props.supabase.auth.signOut()
+//   if (error) console.error(error)
+// }
+//
+</script>
+
+<template>
+  <nav class="container flex py-4">
+    <RouterLink class="flex" to="/">
+      <Logo class="m-auto -ml-1 text-2xl" />
+      <div class="my-auto ml-2">
+        <div class="text-xl">
+          Windi Share Library
+          <sup class="text-xs opacity-50 italic">Beta</sup>
+        </div>
+      </div>
+      <!-- <button @click="login">login</button>
+      <button @click="logout">logout</button>-->
+    </RouterLink>
+  </nav>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,61 +1,102 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { useI18n } from 'vue-i18n'
+import { defineProps, ref, onMounted } from 'vue'
+import type { PropType } from 'vue'
+import type { Config } from 'windicss/types/interfaces'
+import { createClient } from '@supabase/supabase-js'
+import { useWindiCSS } from '~/logic'
 
-const name = ref('')
+// import { useRouter } from 'vue-router'
+// import { useI18n } from 'vue-i18n'
+// const name = ref('')
+// const router = useRouter()
+// // const go = () => {
+// //   if (name.value)
+// //     router.push(`/hi/${encodeURIComponent(name.value)}`)
+// // }
+// const { t } = useI18n()
 
-const router = useRouter()
-const go = () => {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+const props = defineProps({
+  config: {
+    type: Object as PropType<Config>,
+  },
+})
+
+type windiComponent = {
+  id: string
+  html: string
+  css: string
+  likes: number,
+  username: string,
+  uploaded_at: string
 }
 
-const { t } = useI18n()
+const supabaseUrl = 'https://azrzntdriayvwuqzxvth.supabase.co'
+const supabaseKey = ""
+const supabase = createClient(supabaseUrl, supabaseKey)
+const componentsArray = ref<windiComponent[]>([])
+onMounted(async () => {
+  const { data, error } = await supabase
+    .from('components')
+    .select('*')
+    .order('likes', { ascending: false })
+  if (error) console.error(error)
+  data?.forEach((component: windiComponent) => {
+    console.log(data)
+    componentsArray.value.push(component)
+  })
+})
+
+// INSERT NEW ENTRY
+// const { data, error } = await supabase
+//   .from('components')
+//   .insert([
+//     { some_column: 'someValue', other_column: 'otherValue' },
+//   ])
+
+// UPDATE ROW (LIKES)
+// const { data, error } = await supabase
+//   .from('components')
+//   .update({ other_column: 'otherValue' })
+//   .eq('some_column', 'someValue')
 </script>
 
 <template>
-  <div>
-    <p class="text-4xl">
-      <carbon-campsite class="inline-block" />
-    </p>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em class="text-sm opacity-75">{{ t('intro.desc') }}</em>
-    </p>
-
-    <div class="py-4" />
-
-    <input
-      id="input"
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      class="px-4 py-2 text-sm text-center bg-transparent border border-gray-200 rounded outline-none active:outline-none dark:border-gray-700"
-      style="width: 250px"
-      @keydown.enter="go"
+  <div class="grid gap-5 grid-cols-4 +sm:grid-cols-1 +md:grid-cols-2 +lg:grid-cols-3">
+    <div
+      v-for="component in componentsArray"
+      :key="component.id"
+      class="rounded flex flex-col bg-gray-400 h-350px w-350px items-center justify-center"
     >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
+      <div
+        id="upperpart"
+        style="flex: 4;"
+        class="flex h-full w-full p-2 justify-center items-center"
       >
-        {{ t('button.go') }}
-      </button>
+        <Iframe
+          class="h-full w-full"
+          :html="ref(component.html)"
+          :css="ref(useWindiCSS(ref(component.html), ref(component.css), props.config).generatedCSS)"
+        />
+      </div>
+      <div
+        id="lowerpart"
+        class="cursor-pointer flex-grow bg-gray-300 flex-1 p-2 place-self-stretch self-stretch justify-self-stretch hover:bg-gray-50 hover:ring"
+      >
+        <div>
+          uploaded by
+          <span class="text-gray-500">{{ component.username }}</span>
+          at
+          <span class="text-gray-500">
+            {{
+              new Date(component.uploaded_at).toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: '2-digit' })
+            }}
+          </span>
+        </div>
+        <span class="flex items-center justify-center select-none">
+          <ri:heart-2-fill class="mr-2 text-red-600" />
+          {{ component.likes }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: home
-</route>
