702fc3969927385832f7aad3d14b6248f6e8c65a
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-04-01 14:01:07 +0200

    fix: iframe loading

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -28,41 +28,43 @@ onMounted(async () => {
     </router-link>
   </div>
   <div class="grid gap-5 grid-cols-4 +sm:grid-cols-1 +md:grid-cols-2 +lg:grid-cols-3">
-    <div
-      v-for="component in allComponents"
-      :key="component.id"
-      class="rounded flex flex-col bg-gray-400 h-350px w-350px items-center justify-center"
-    >
+    <ClientOnly>
       <div
-        id="upperpart"
-        style="flex: 4;"
-        class="flex h-full w-full p-2 justify-center items-center"
+        v-for="component in allComponents"
+        :key="component.id"
+        class="rounded flex flex-col bg-gray-400 h-350px w-350px items-center justify-center"
       >
-        <Iframe
-          class="h-full w-full"
-          :html="ref(component.html)"
-          :css="ref(useWindiCSS(ref(component.html), ref(component.css), props.config).generatedCSS)"
-        />
-      </div>
-      <div
-        id="lowerpart"
-        class="cursor-pointer flex-grow bg-gray-300 flex-1 p-2 place-self-stretch self-stretch justify-self-stretch hover:bg-gray-50 hover:ring"
-      >
-        <div>
-          uploaded by
-          <span class="text-gray-500">{{ component.user_name }}</span>
-          at
-          <span class="text-gray-500">
-            {{
-              new Date(component.added_at).toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: '2-digit' })
-            }}
+        <div
+          id="upperpart"
+          style="flex: 4;"
+          class="flex h-full w-full p-2 justify-center items-center"
+        >
+          <Iframe
+            class="h-full w-full"
+            :html="ref(component.html)"
+            :css="ref(useWindiCSS(ref(component.html), ref(component.css), props.config).generatedCSS)"
+          />
+        </div>
+        <div
+          id="lowerpart"
+          class="cursor-pointer flex-grow bg-gray-300 flex-1 p-2 place-self-stretch self-stretch justify-self-stretch hover:bg-gray-50 hover:ring"
+        >
+          <div>
+            uploaded by
+            <span class="text-gray-500">{{ component.user_name }}</span>
+            at
+            <span class="text-gray-500">
+              {{
+                new Date(component.added_at).toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: '2-digit' })
+              }}
+            </span>
+          </div>
+          <span class="flex items-center justify-center select-none">
+            <ri:heart-2-fill class="mr-2 text-red-600" />
+            {{ component.likes }}
           </span>
         </div>
-        <span class="flex items-center justify-center select-none">
-          <ri:heart-2-fill class="mr-2 text-red-600" />
-          {{ component.likes }}
-        </span>
       </div>
-    </div>
+    </ClientOnly>
   </div>
 </template>
