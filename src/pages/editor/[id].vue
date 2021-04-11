c64eaa439782b107e53d2ded4f109d7f14bc82aa
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-04-11 17:17:42 +0200

    feat: editor with id

---
+++ src/pages/editor/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+<h1>
+  {{props.id}}
+</h1>
